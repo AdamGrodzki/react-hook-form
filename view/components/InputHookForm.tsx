364e753db21470ad326ae5cc731716b97fac95da
Adam Grodzki <110930032+AdamGrodzki@users.noreply.github.com>
2024-02-13 08:18:09 +0100

    add variables.scss to inputHookForm

--- view/components/InputHookForm.tsx
+++ view/components/InputHookForm.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import * as React from 'react';
 import { useController, UseControllerProps } from "react-hook-form";
 import { IFormInput } from "@/app/page";
 import { clsx } from "clsx";
